7777fa276900b5ad18890c0a35fc6368f072db76
EC2 Default User <ec2-user@ip-172-31-39-11.ec2.internal>
2021-08-06 06:20:51 +0000

    Initial8b

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,65 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  
+
+
+  def show
+    @book = Book.find(params[:id])
+    @book_comment = BookComment.new
+  end
+
+  def index
+   if params[:button].to_i == 1
+      @books = Book.all.order(id: "ASC")
+   elsif params[:button].to_i == 2
+       @books = Book.all.order(rate: "DESC")
+   else
+     @books = Book.all
+   end
+   @book = Book.new
+  end
+  
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :rate)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
+   
+end
+
